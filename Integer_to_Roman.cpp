1ca2bd3de0251a7229aa7dbe1a4b76a8fbc1eb10
ekra024 <ekra.cse8.bu@gmail.com>
2026-01-15 19:27:53 +0600

    day 14

--- Integer_to_Roman.cpp
+++ Integer_to_Roman.cpp
@@ -7,51 +7,54 @@ int main()
     string ans = "";
 
     while(num > 0) {
-        string s = to_string(num);
-
-        if(s.size() == 4) {
-            if(s[0] == '1') ans += "M";
-            else if(s[0] == '2') ans +="MM";
-            else ans += "MMM";
-            num = num % 1000;
+        if(num >= 1000) {
+            ans += 'M';
+            num -= 1000;
         }
-        else if(s.size() == 3) {
-            if(s[0] == '1') ans += "C";
-            else if(s[0] == '2') ans += "CC";
-            else if(s[0] == '3') ans += "CCC";
-            else if(s[0] == '4') ans += "CD";
-            else if(s[0] == '5') ans += "D";
-            else if(s[0] == '6') ans += "DC";
-            else if(s[0] == '7') ans += "DCC";
-            else if(s[0] == '8') ans += "DCCC";
-            else if(s[0] == '9') ans += "CM";
-            num = num % 100;
+        else if(num >= 900) {
+            ans += "CM";
+            num -= 900;
         }
-        else if(s.size() == 2) {
-            if(s[0] == '1') ans += "X";
-            else if(s[0] == '2') ans += "XX";
-            else if(s[0] == '3') ans += "XXX";
-            else if(s[0] == '4') ans += "XL";
-            else if(s[0] == '5') ans +=  "L";
-            else if(s[0] == '6') ans += "LX";
-            else if(s[0] == '7') ans += "LXX";
-            else if(s[0] == '8') ans += "LXXX";
-            else if(s[0] == '9') ans += "XC";
-            num = num % 10;
+        else if(num >= 500) {
+            ans += 'D';
+            num -= 500;
         }
-        else{
-            if(s[0] == '1') ans += "I";
-            else if(s[0] == '2') ans += "II";
-            else if(s[0] == '3') ans += "III";
-            else if(s[0] == '4') ans += "IV";
-            else if(s[0] == '5') ans += "V";
-            else if(s[0] == '6') ans += "VI";
-            else if(s[0] == '7') ans += "VII";
-            else if(s[0] == '8') ans += "VIII";
-            else if(s[0] == '9') ans += "IX";
-            num = num % 1;
+        else if(num >= 100){
+            ans += 'C';
+            num -= 100;
         }
-
+        else if(num >= 90) {
+            ans += "XC";
+            num -= 90;
+        }
+        else if(num >= 50) {
+            ans += 'L';
+            num -= 50;
+        }
+        else if(num >= 40) {
+            ans += "XL";
+            num -= 40;
+        }
+        else if(num >= 10) {
+            ans += 'X';
+            num -= 10;
+        }
+        else if(num >= 9) {
+            ans += "IX";
+            num -= 9;
+        }
+        else if(num >= 5) {
+            ans += 'V';
+            num -= 5;
+        }
+        else if(num >= 4) {
+            ans += "IV";
+            num -= 4;
+        }
+        else if(num >= 1) {
+            ans += 'I';
+            num -= 1;
+        }
     }
 
     cout << ans;
--- Longest_Common_Prefix.cpp
+++ Longest_Common_Prefix.cpp
@@ -10,7 +10,7 @@ int main()
     }
 
     string s = strs[0];
-    int k = s.size(), cnt = 0, mx = 0;
+    int k = s.size();
     string ans ="";
 
     for(int i = 0; i < k; i++) {
