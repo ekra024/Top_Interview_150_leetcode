e9c95c4d1a982804446f8b3ff43eb36d23850111
ekra024 <ekra.cse8.bu@gmail.com>
2026-01-19 20:25:29 +0600

    day 15

--- Gas_Station.cpp
+++ Gas_Station.cpp
@@ -3,47 +3,34 @@ using namespace std;
 
 int main()
 {
-    int n; cin >> n;
+    int n;
+    cin >> n;
     vector<int>gas(n),cost(n);
     for(int i = 0; i < n; i++)cin >> gas[i];
     for(int i = 0; i < n; i++) cin >> cost[i];
 
-    int totalCost = accumulate(cost.begin(), cost.end(), 0);
-        long long mnCost = totalCost;
-        int idx = -1;
-        if(gas[0] >= cost[0]) {
-            mnCost -= cost[n-1];
-            idx = 0;
+    int idx = 0, totalGas = 0;
+        for (int i = 0; i < n; i++) {
+            totalGas += (gas[i] - cost[i]);
         }
 
-        for (int i = 1; i < n; i++) {
-            long long curCost = totalCost - cost[i - 1];
-            if (gas[i] >= cost[i] && curCost < mnCost) {
-                mnCost = curCost;
-                idx = i;
+        if (totalGas < 0) {
+            cout << -1 <<'\n';
+            return 0;
+        }
+
+
+
+        int curGas = 0;
+        for (int i = 0; i < n; i++) {
+            curGas += (gas[i] - cost[i] );
+            if(curGas < 0) {
+                curGas = 0;
+                idx = i+1;
             }
         }
-        if(idx == -1) {
-            cout<< "---1";return 0;
-        }
-        cout << idx <<'\n';
-        int curGas = gas[idx];
-        int tmp = idx;
-        while (tmp != idx - 1) {
 
-            cout << curGas <<' '<< cost[tmp]<<'\n';
-            if (curGas < cost[tmp]) {
-            cout<< "--1";return 0;
-        }
-            curGas = curGas - cost[tmp] + (gas[(tmp + 1) % n]);
-            tmp = ((tmp + 1) % n);
-        }
+    cout << idx <<'\n';
 
-        if (curGas < cost[idx - 1]) {
-            cout << curGas <<'\n';
 
-            cout<< "-1";return 0;
-
-        }
-         cout <<idx;
 }
